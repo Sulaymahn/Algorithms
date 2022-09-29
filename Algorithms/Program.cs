eacf9152f7dcdee35c71471d2b5b88c88e23dd01
Sulaymahn <sulmuk28@gmail.com>
2022-09-29 17:56:34 +0100

    Add project files.

---
+++ Algorithms/Program.cs
@@ -0,0 +1,13 @@
+﻿using Algorithms.Helpers;
+using Algorithms.Models;
+using Algorithms.Sort;
+using System;
+
+var seqToSort = new int[]
+{
+    1, 3, 7, 0, -3, -2, 8
+};
+
+SortBase sortAlgo = new Insertion();
+sortAlgo.Sort(seqToSort);
+seqToSort.Print();
---
+++ AlgorithmsLibrary/Sort/Insertion.cs
@@ -0,0 +1,26 @@
+﻿using AlgorithmsLibrary.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLibrary.Sort
+{
+    internal class Insertion : SortBase
+    {
+        public override void Sort(int[] seq)
+        {
+            for (int i = 1; i < seq.Length; i++)
+            {
+                int key = seq[i];
+                int sortedIndex = i - 1;
+                while (sortedIndex >= 0 && seq[sortedIndex] > key)
+                {
+                    seq[i] = seq[sortedIndex];
+                    sortedIndex--;
+                }
+                seq[sortedIndex + 1] = key;
+            }
+        }
+    }
+}
