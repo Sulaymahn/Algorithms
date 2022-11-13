acb329374435db45889f5cfa5f77e9559f76f355
Sulaymahn Mukhtar <sulmuk28@gmail.com>
2022-11-13 21:04:02 +0100

    Cramer's rule

---
+++ AlgorithmsLibrary/Math/Matrix.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsLibrary.Math
+{
+    public static class Matrix
+    {
+        public static decimal CramerDeterminant(int[,] arr)
+        {
+            if(arr.GetLength(0) != arr.GetLength(1))
+            {
+                throw new ArgumentException("Array has to be square");
+            }
+
+            if (arr.GetLength(0) == 2)
+            {
+                return (arr[0, 0] * arr[1, 1]) - (arr[1, 0] * arr[0, 1]);
+            }
+
+            decimal result = 0;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                result += (int)System.Math.Pow(-1, i + 2) * arr[0, i] * CramerDeterminant(GetTrunc(i, arr));
+            }
+            return result;
+        }
+        static int[,] GetTrunc(int column, int[,] arr)
+        {
+            int size = arr.GetLength(0);
+            var result = new int[size - 1, size - 1];
+            for (int x = 0; x < result.GetLength(0); x++)
+            {
+                int offset = 0;
+                for (int y = 0; y < result.GetLength(0); y++)
+                {
+                    if (y == column)
+                    {
+                        offset++;
+                    }
+                    result[x, y] = arr[x + 1, y + offset];
+                }
+            }
+            return result;
+        }
+
+        public static void Print(int[,] arr)
+        {
+            for (int x = 0; x < arr.GetLength(0); x++)
+            {
+                for (int y = 0; y < arr.GetLength(0); y++)
+                {
+                    Console.Write($"{arr[x, y]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static int[,] Generate(int size)
+        {
+            int[,] result = new int[size, size];
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    result[x, y] = Random.Shared.Next(0, 5);
+                }
+            }
+            return result;
+        }
+    }
+}
--- AlgorithmsPlayground/Program.cs
+++ AlgorithmsPlayground/Program.cs
@@ -1,17 +1,37 @@
-﻿using AlgorithmsLibrary.Sort;
+﻿using AlgorithmsLibrary.Math;
+using System.Timers;
 
-var sorter = new Insertion();
-var rnd = new Random();
-var seq = new int[rnd.Next(100)];
-
-for (int i = 0; i < seq.Length; i++)
+int[,] array1 = new int[4, 4]
 {
-    seq[i] = rnd.Next();
-}
+    { 1, 0, 3, 2},
+    { 0, 1, -1, 0},
+    { 2, 2, 1, 1},
+    { -1, 0, 1, 3}
+};
 
-sorter.Sort(seq);
+System.Numerics.Matrix4x4 matrix = new(1, 0, 3, 2, 0, 1, -1, 0, 2, 2, 1, 1, -1, 0, 1, 3);
 
-foreach (var item in seq)
+int[,] array2 = new int[3, 3]
 {
-    Console.WriteLine(item);
+    { 6, 2, -4},
+    { 5, 6, -2},
+    { 5, 2, -3}
+};
+int[,] array3 = new int[2, 2]
+{
+    { 6, -2},
+    { 2, -3}
+};
+
+for (int i = 0; i < 2000; i++)
+{
+    int start = Environment.TickCount;
+    decimal result = (int)matrix.GetDeterminant();
+    int end = Environment.TickCount;
+    Console.WriteLine($"{result} in {end - start}ms (4x4)");
+
+    int start1 = Environment.TickCount;
+    decimal result1 = Matrix.CramerDeterminant(array1);
+    int end1 = Environment.TickCount;
+    Console.WriteLine($"{result1} in {end1 - start1}ms (Array)");
 }
